9fc92f787954b704e4fd359e46a6b73bb58a1ab3
Fabio Oliveira <fabio20rj@gmail.com>
2018-10-20 22:58:46 -0300

    fix allowed_hosts

--- mtnomundao/settings.py
+++ mtnomundao/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'heroku.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mtnomundao.herokuapp.com']
 
 
 # Application definition
